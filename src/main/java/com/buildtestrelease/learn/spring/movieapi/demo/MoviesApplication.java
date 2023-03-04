453328bb6304b80633a08dd1babdc8bedfc89a1b
vikasbhardwaj <vikasbhardwaj@localhost>
2023-03-04 14:14:23 +1100

    Movie Service working. Pending Unit, Integration and automation tests

--- src/main/java/com/buildtestrelease/learn/spring/movieapi/demo/MoviesApplication.java
+++ src/main/java/com/buildtestrelease/learn/spring/movieapi/demo/MoviesApplication.java
@@ -2,12 +2,15 @@ package com.buildtestrelease.learn.spring.movieapi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class MoviesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MoviesApplication.class, args);
 	}
+	
 
 }
